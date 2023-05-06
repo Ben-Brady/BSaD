49ce989cd0e049f1ebd69769effe261bb7afcdef
Ben Brady <benbradybusiness@gmail.com>
2023-05-06 21:54:44 +0100

    Update

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+from endpoints import inject_routes
+import jwt
 
-app = Flask(__name__)
+
+app = Flask(
+  import_name=__name__,
+  template_folder="templates",
+  static_folder="static",
+)
 
 
 @app.route('/')
 def index():
   return render_template("index.html")
 
+inject_routes(app)
 
-app.run(host='0.0.0.0', port=81)
+app.run(
+  host='0.0.0.0',
+  port=8081,
+  debug=True,
+)
